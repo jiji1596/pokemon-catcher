d918f6b5d2d5c7746c8bbaca1c6891575c3af844
Shunji Sugaya <sugayasc33@gmail.com>
2025-07-04 17:03:19 +0800

    edited the storage css

--- src/components/sections/StoredPokemonList.jsx
+++ src/components/sections/StoredPokemonList.jsx
@@ -5,27 +5,28 @@ export const StoredPokemonList = () => {
 
   return (
     <div className="flex flex-col justify-center items-center w-full">
-      <h2 className="text-3xl md:text-5xl text-primary text-shadow-neutral-700 text-center">
+      <h2 className="text-3xl md:text-5xl text-primary text-shadow-neutral-700 text-center my-8">
         Storage
       </h2>
-      <div className="grid grid-cols-3 gap-3">
+      <div className="grid grid-cols-4 gap-4">
         {storedPokemons.length > 0 &&
           storedPokemons.map((pokemon, key) => {
             return (
-              <div className="flex grow-1" key={key}>
-                <img className="h-20 w-20 mx-1" src={pokemon.image} alt="" />
-                <div className="flex flex-col items-start">
-                  <h3 className="text-xl font-semibold text-center pb-1 capitalize">
-                    {pokemon.name}
-                  </h3>
-                  <ul className="grid grid-cols-1 gap-2 pb-3">
-                    {pokemon.types.map((type, i) => (
-                      <li key={i}>
-                        <img className="h-4 w-16 my-0" src={type} alt="" />
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+              <div
+                className="flex flex-col justify-center items-center p-5 bg-white border-4 rounded-3xl"
+                key={key}
+              >
+                <h3 className="text-3xl font-semibold text-center pb-1 capitalize">
+                  {pokemon.name}
+                </h3>
+                <img className="h-40 w-40 mx-1" src={pokemon.image} alt="" />
+                <ul className="flex gap-2">
+                  {pokemon.types.map((type, i) => (
+                    <li key={i}>
+                      <img className="h-4 w-16 my-0" src={type} alt="" />
+                    </li>
+                  ))}
+                </ul>
               </div>
             );
           })}
